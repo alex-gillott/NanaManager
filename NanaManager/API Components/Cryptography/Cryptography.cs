3b64c1b39b9009459e577c74e1f83c5aa06a943d
Hydroxa <alexgillott489@gmail.com>
2020-12-10 14:17:56 +0000

    Commented Cryptography.cs

--- NanaManager/API Components/Cryptography/Cryptography.cs
+++ NanaManager/API Components/Cryptography/Cryptography.cs
@@ -8,22 +8,22 @@ namespace NanaManager.Cryptography
     /// </summary>
     public class Cryptography : ICryptographyProvider
     {
-        public void Initialise( string Key ) {
+        public void Initialise( string Key ) { //Required for the Cryptography Provider
         }
 
-        public void Terminate() {
+        public void Terminate() { //Required for the Cryptography Provider
         }
 
         public byte[] Encrypt( byte[] Data ) {
-            return Data;
+            return Data; //Don't encrypt
         }
 
-        public string Encrypt( string PlainText ) => Encoding.UTF8.GetString( Encrypt( Encoding.UTF8.GetBytes( PlainText ) ) );
+        public string Encrypt( string PlainText ) => Encoding.UTF8.GetString( Encrypt( Encoding.UTF8.GetBytes( PlainText ) ) ); //CEncrypt as a byte array, and return as a string
 
         public byte[] Decrypt( byte[] Data ) {
-            return Data;
+            return Data; //Nothing to decrypt
         }
 
-        public string Decrypt( string Data ) => Encoding.UTF8.GetString( Decrypt( Encoding.UTF8.GetBytes( Data ) ) );
+        public string Decrypt( string Data ) => Encoding.UTF8.GetString( Decrypt( Encoding.UTF8.GetBytes( Data ) ) ); //Decrypt as a byte array, and return as a string
     }
 }
